6106c4d84d7dab42de98e5da212e8f96fc6ee86c
jishnut <jishnut311@gmail.com>
2014-05-30 16:06:24 +0530

    Used AngularJS in Billing and Shipping

---
+++ Design Docs/UI Design/Billing and Shipping/js/main.js
@@ -0,0 +1,19 @@
+function hvr(id)
+{
+	//id.style.background-color = 'red';
+	id.style.background="lightcyan";
+}
+
+function out(id)
+{
+	//id.style.background-color = 'red';
+	id.style.background="aliceblue";
+}
+
+function ctrl1($scope)
+{
+$scope.cases=[
+{heading:'BILLING', ti:'', ln:'Jose', fn:'Nithin', al1:'Google Australia Pty Ltd.', al2: 'Level 5, 48 Pirrama Road', al3: 'Pyrmount', al4:'', pc:'2009', occ:'au', st:'New South Wales,Australia', cs:'Pyrmount', cn:'Google Sydney', cne:'', gt1:'121231312', mob:'123123213'},
+{heading:'SHIPPING', ti:'', ln:'Jose', fn:'Nithin', al1:'Google Australia Pty Ltd.', al2: 'Level 5, 48 Pirrama Road', al3: 'Pyrmount', al4:'', pc:'2009', occ:'au', st:'New South Wales,Australia', cs:'Pyrmount', cn:'Google Sydney', cne:'', gt1:'121231312', mob:'123123213'}]
+
+}
