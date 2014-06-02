47f7a1def4518214a4eb3a47f91a5dc69ef79360
thankachankarun639 <karunthankachan1@gmail.com>
2014-06-02 12:30:33 +0530

    Consolidate Desgin

---
+++ Design Docs/UI Design/Consolidated Design/js/hover.js
@@ -0,0 +1,11 @@
+function hvr(id)
+{
+	//id.style.background-color = 'red';
+	id.style.background="lightcyan";
+}
+
+function out(id)
+{
+	//id.style.background-color = 'red';
+	id.style.background="aliceblue";
+}
