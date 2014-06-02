ad71becbaa218857fb11647a1e8aa0cd3d959100
thankachankarun639 <karunthankachan1@gmail.com>
2014-06-02 11:03:21 +0530

    Alteration to web candidate

--- Design Docs/UI Design/Export/js/main.js
+++ Design Docs/UI Design/Export/js/main.js
@@ -1,5 +1,20 @@
+/*$("#toggle_image_1").css({
+        "-webkit-transform": "rotate(180deg)",
+        "-moz-transform": "rotate(180deg)",
+        "-ms-transform": "rotate(180deg)",
+								});
+								
+$("#toggle_image_2").css({
+        "-webkit-transform": "rotate(180deg)",
+        "-moz-transform": "rotate(180deg)",
+        "-ms-transform": "rotate(180deg)",
+								});
+		
+*/
 var keeper=1;
 var keeper_2=1;
+
+
 
 $('#toggle_image_1').click(function () {
 
@@ -7,34 +22,44 @@ $('#toggle_image_1').click(function () {
 	{
     	$('#toggle_1').slideDown('slow');
 		keeper=1;
+		$("#toggle_image_1").css({
+        "-webkit-transform": "rotate(0deg)",
+        "-moz-transform": "rotate(0deg)",
+        "-ms-transform": "rotate(0deg)"
+								});
 	}
 	else if(keeper==1)
 	{
 	 $('#toggle_1').slideUp('slow');
 	 keeper=0;
+	 $("#toggle_image_1").css({
+        "-webkit-transform": "rotate(180deg)",
+        "-moz-transform": "rotate(180deg)",
+        "-ms-transform": "rotate(180deg)"
+    });
 	}
 });
 
+
 $('#toggle_image_2').click(function () {
-
-		$(this).css({
-       "-webkit-transform": "+rotate(180deg)",
-        "-moz-transform": "+rotate(180deg)",
-        "transform": "+rotate(180deg)" /* For modern browsers(CSS3)  */
-    });
-
 	if(keeper_2==0)
 	{
     	$('#toggle_2').slideDown('slow');
 		keeper_2=1;
+		$("#toggle_image_2").css({
+       "-webkit-transform": "rotate(0deg)",
+        "-moz-transform": "rotate(0deg)",
+        "-ms-transform": "rotate(0deg)" 
+								})
 	}
 	else if(keeper_2==1)
 	{
 	 $('#toggle_2').slideUp('slow');
 	 keeper_2=0;
+		$("#toggle_image_2").css({
+       "-webkit-transform": "rotate(180deg)",
+        "-moz-transform": "rotate(180deg)",
+        "-ms-transform": "rotate(180deg)" 
+								})
 	}
-});
-
-$('img').click(function(){
-    
 });
