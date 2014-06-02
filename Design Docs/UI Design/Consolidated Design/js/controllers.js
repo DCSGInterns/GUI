669d63610e0f2390724388a28419fea70a7ea628
jishnut <jishnut311@gmail.com>
2014-06-02 17:24:29 +0530

    added index

--- Design Docs/UI Design/Consolidated Design/js/controllers.js
+++ Design Docs/UI Design/Consolidated Design/js/controllers.js
@@ -4,5 +4,26 @@ GcmEmeaApp.controller('DataCtrl', function ($scope, $http) {
   $http.get('data/FormData.json').success(function(data) {
     $scope.formData = data;
   });
+  });
+  GcmEmeaApp.controller('ctrl1',function($scope) {
+  $scope.cases=[
+{heading:'BILLING', ti:'', ln:'Jose', fn:'Nithin', al1:'Google Australia Pty Ltd.', al2: 'Level 5, 48 Pirrama Road', al3: 'Pyrmount', al4:'', pc:'2009', occ:'au', st:'New South Wales,Australia', cs:'Pyrmount', cn:'Google Sydney', cne:'', gt1:'121231312', mob:'123123213'},
+{heading:'SHIPPING', ti:'', ln:'Jose', fn:'Nithin', al1:'Google Australia Pty Ltd.', al2: 'Level 5, 48 Pirrama Road', al3: 'Pyrmount', al4:'', pc:'2009', occ:'au', st:'New South Wales,Australia', cs:'Pyrmount', cn:'Google Sydney', cne:'', gt1:'121231312', mob:'123123213'}]
 
-});
+  });
+  GcmEmeaApp.controller('ctrl2',function($scope) {
+  $scope.fc=1;
+$scope.ard='up';
+$scope.clk=function(){
+if($scope.fc==1)
+{$scope.fc=0;}
+else
+{
+if($scope.ard=='up')
+{$scope.ard='down';}
+else
+{$scope.ard='up';}
+//if($scope.ard=='up')
+//{}
+}
+} });
---
+++ Design Docs/UI Design/Consolidated Design/js/main.js
@@ -0,0 +1,15 @@
+function hvr(id)
+{
+	//id.style.background-color = 'red';
+	id.style.background="lavender";
+}
+
+function out(id)
+{
+	//id.style.background-color = 'red';
+	id.style.background="aliceblue";
+}
+
+
+//document.getElementsByClassName("form-group").onmouseover="hvr(this)";
+//document.getElementsByClassName("form-group").onmouseout="out(this)";
